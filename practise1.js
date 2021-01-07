842c9479ef55f5b0bb178800a4d687159c0e5637
[cagataysen] <[drosen_26@hotmail.com]>
2021-01-07 20:27:50 +0300

    last version

--- practise1.js
+++ practise1.js
@@ -3,18 +3,18 @@ const http = require("http");
 const url = require("url");
 
 const server = http.createServer((req, res) => {
-    console.log(req.url);
-
     const pathName = req.url;
 
-    if(pathName === "/overview") {
+    if(pathName === "/" || pathName === "/overview") {
         res.end("This is the OVERVIEW");
-    } else if (pathName === "/product") {
+    }
+    else if (pathName === "/product") {
         res.end("This is the PRODUCT");
     }
+    else {
+        res.end("Page not Found!");
+    }
     
-
-    res.end("Hello from the server!");
 });
 
 server.listen(8000, "127.0.0.1", () => {
