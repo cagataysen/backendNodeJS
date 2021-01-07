66ad147551df833d04d7c045e19aba7ab8470425
[cagataysen] <[drosen_26@hotmail.com]>
2021-01-07 21:02:40 +0300

    api created

--- practise1.js
+++ practise1.js
@@ -2,6 +2,10 @@ const fs = require("fs");
 const http = require("http");
 const url = require("url");
 
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+    
+
 const server = http.createServer((req, res) => {
     const pathName = req.url;
 
@@ -11,8 +15,16 @@ const server = http.createServer((req, res) => {
     else if (pathName === "/product") {
         res.end("This is the PRODUCT");
     }
+    else if (pathName === "/api") {
+            res.writeHead(200, { "Content-type": "application/json"});
+            res.end(data);        
+    }
     else {
-        res.end("Page not Found!");
+        res.writeHead(404, {
+            "Content-type": "text/html"
+            
+        });
+        res.end("<h1>Page not Found!</h1>");
     }
     
 });
