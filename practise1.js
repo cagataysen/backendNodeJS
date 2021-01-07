7343664a014587e4101d565443d0500374e5b431
[cagataysen] <[drosen_26@hotmail.com]>
2021-01-07 16:18:54 +0300

    server created

--- practise1.js
+++ practise1.js
@@ -1,9 +1,25 @@
 const fs = require("fs");
 const http = require("http");
+const url = require("url");
 
-http.createServer((req, res) => {
+const server = http.createServer((req, res) => {
+    console.log(req.url);
+
+    const pathName = req.url;
+
+    if(pathName === "/overview") {
+        res.end("This is the OVERVIEW");
+    } else if (pathName === "/product") {
+        res.end("This is the PRODUCT");
+    }
+    
+
     res.end("Hello from the server!");
-})
+});
+
+server.listen(8000, "127.0.0.1", () => {
+    console.log("Listening to requests on port 8000");
+});
 
 /* 
 PART 4 THESE WERE ALL FOR FILES NO SERVER
