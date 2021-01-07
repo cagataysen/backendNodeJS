9509deb4b1d89600fddb1057355f0a13ce9b91a8
[cagataysen] <[drosen_26@hotmail.com]>
2021-01-07 22:50:46 +0300

    tempFilled

--- practise1.js
+++ practise1.js
@@ -4,6 +4,20 @@ const url = require("url");
 
 
 //Server
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+
+    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+    return output;
+}
+
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, "utf-8");
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, "utf-8");
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, "utf-8");
@@ -19,6 +33,12 @@ const server = http.createServer((req, res) => {
     //Overview page 
     if(pathName === "/" || pathName === "/overview") {
         res.writeHead(200, {"Content-type": "text/html"});
+
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el));
+        console.log(cardsHtml);
+
+
+    
         res.end(tempOverview);
 
     //Product Page 
